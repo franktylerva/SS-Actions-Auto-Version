2c600b62083d8e0437b692ca3b4c5b13d0a66280
ftyler <franktylerva@gmail.com>
2022-03-18 14:37:14 -0400

    feat: Change the output.

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ from app import routers
 
 app = FastAPI(
     title="ss-sample-python-api",
-    description="Sample Python API test.",
+    description="Sample Python API test Test.",
     version="v1",
 )
 
